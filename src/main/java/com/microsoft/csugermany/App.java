2ae1471be91363b673863772be2ecd486f096029
Jeffrey Groneberg <inkvine@gmx.net>
2025-05-19 17:10:02 +0200

    Skeleton

---
+++ src/main/java/com/microsoft/csugermany/App.java
@@ -0,0 +1,11 @@
+package com.microsoft.csugermany;
+
+public class App {
+    public static void main(String[] args) {
+
+        // This is the main method where the application starts
+        // You can add your application logic here
+        System.out.println("Hello, Star Wars!");
+
+    }
+}
